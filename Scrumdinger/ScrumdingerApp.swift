2c5c6d434575d6debcb5736ddd945855fa98b5b9
Joseph DeWeese <rangerboy901@icloud.com>
2023-04-22 15:18:28 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joseph DeWeese on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

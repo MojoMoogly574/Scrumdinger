e8b32b6d91326ad3256a02c00d6f3afef85bd675
Joseph DeWeese <rangerboy901@icloud.com>
2023-05-07 13:36:14 -0500

    UI corrections and improvements

--- Scrumdinger/Views/Screens/WorkoutDetailScreen.swift
+++ Scrumdinger/Views/Screens/WorkoutDetailScreen.swift
@@ -14,40 +14,41 @@ struct WorkoutDetailScreen: View {
     
     var body: some View {
         List {
-            NavigationLink(destination: WorkoutTimerScreen()) {
-                Section(header: Text("Workout Info")) {
+            Section(header: Text("Workout Info")) {
+            NavigationLink(destination: WorkoutSessionScreen()) {
+                
                     Label("Start Workout", systemImage: "timer")
                         .font(.headline)
                         .foregroundColor(.accentColor)
                 }
-                           HStack {
-                               Label("Length", systemImage: "clock")
-                               Spacer()
-                               Text("\(workout.lengthInMinutes) minutes")
-                           }
-                           .accessibilityElement(children: .combine)
-                           HStack {
-                               Label("Theme", systemImage: "paintpalette")
-                               Spacer()
-                                                   Text(workout.theme.name)
-                                                       .padding(4)
-                                                       .foregroundColor(workout.theme.accentColor)
-                                                       .background(workout.theme.mainColor)
-                                                       .cornerRadius(4)
-                                               }
-                                               .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(workout.lengthInMinutes) minutes")
                 }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(workout.theme.name)
+                        .padding(4)
+                        .foregroundColor(workout.theme.accentColor)
+                        .background(workout.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+                
             }
             Section(header:  Text("Exercises")) {
                 ForEach(workout.exercises) { exercise in
-                                    Label(exercise.name, systemImage: "figure.strengthtraining.traditional")
-                                }
-               
-                            }
-         
-                        }
-                    }
+                    Label(exercise.name, systemImage: "figure.strengthtraining.traditional")
+                }
                 
+            }
+            
+        }
+    }
+}
 
 struct WorkoutDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
